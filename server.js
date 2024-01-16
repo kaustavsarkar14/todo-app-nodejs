060874aecf37b19defb878d486ed982145108c4c
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-16 15:38:48 +0530

    register + login routes + ejs

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+let ejs = require("ejs");
+require("dotenv").config();
 
+// constants
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.listen(8000, ()=>{
-    console.log("server is running")
-})
+// middlewares
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.post("/register", (req, res) => {
+  res.send(req.body);
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", (req, res) => {
+  res.send("login suceess");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running on : " + PORT);
+});
